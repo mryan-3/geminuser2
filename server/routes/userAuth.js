1d52b0b3713952ffeb3383244c283798010cd08c
mryan-3 <malawaryan@gmail.com>
2023-11-13 17:50:16 +0300

    Added node modules, added authentication routes using JWT.

--- server/routes/userAuth.js
+++ server/routes/userAuth.js
@@ -5,67 +5,50 @@ const jwt = require("jsonwebtoken")
 const UserAuth = require("../models/userAuthModel")
 const passport = require('passport')
 
-// Google Authentication
-// Login Failed
-router.get('/api/v1/login/failed', (req, res) => {
-    res.status(401).json({
-        error: true, 
-        message: "Login Failed"
-    })
-})
-// Handle authentication flow
-router.get('/api/v1/login', passport.authenticate('google', {
-    successRedirect: '/profile',
-    failureRedirect: '/login',
-  })
-)
-// Protect routes
-router.get('/profile', isLoggedIn, (req, res) => {
-    // Display user profile information
-    const user = req.user;
-    res.render('profile', { user });
-});
-  
-// Check if user is logged in
-function isLoggedIn(req, res, next) {
-    if (!req.isAuthenticated()) {
-        return res.redirect('/login');
-    }
-    next();
+//ACCESS TOKEN GENERATION
+const generateAccessToken = (userId) => {
+    const payload = { userId };
+    const secretKey = 'my-secret-key';
+    const options = { expiresIn: '2h' };
+    return jwt.sign(payload, secretKey, options);
 }
-  
-  // Handle login failure
-router.get('/login', (req, res) => {
-    const error_message = req.flash('error_message');
-    res.render('login', { error_message });
-});
+
+//REFRESH TOKEN GENERATION
+const generateRefreshToken = (userId) => {
+    const payload = { userId };
+    const secretKey = 'my-secret-key';
+    const options = { expiresIn: '7d' };
+    return jwt.sign(payload, secretKey, options);
+};
+
+const blacklistedTokens = [] //Array to store blacklisted tokens
 
 //Registration
-router.post('/register', async(req, res) => {
+router.post('/api/v1/register', async(req, res) => {
     const { username, password } = req.body
 
     try{
         //Check if the username exists
         const existingUser = await UserAuth.findOne({ username })
+        if (!username || !password ) return res.status(400).json({"message" : "Username and Password required"})
         if (existingUser) {
             return res.status(400).json( {error: "Username already taken."})
-        }
+        }   
 
         //Hash the password
         const hashedPassword = await bcrypt.hash(password, 10)
         //Create a new User
-        await User.create({ username, password: hashedPassword})
-        res.json({ message: "User created successfull"})
-
+        const newUser = new User({ username, password: hashedPassword})
+        await newUser.save()
+        res.json({ message: 'User registered successfully' })
     } catch(err){
         console.error(err.message)
         res.status(500).json({error: "Internal Server Error"})
     }
-
 })
 
-//Login
-router.post("/login", async(req, res) => {
+//Login and Authorization
+router.post("/api/v1/login", async(req, res) => {
     const { username, password } = req.body
 
     try{
@@ -77,13 +60,75 @@ router.post("/login", async(req, res) => {
 
         //Compare the passowrd
         const passwordMatch = await bcrypt.compare(password, user.password)
-        if (!passwordMatch){
+        if (passwordMatch){
+            //Generate a JWT
+            const accessToken = generateAccessToken(user._id)
+            const refreshToken = generateRefreshToken(user._id)
+
+            res.json({ accessToken, refreshToken })
+        } else {
             return res.status(401).json({ error: "Invalid password"})
         }
-
-        //Generate a JWT
     } catch(error){
         console.error(error)
         res.status(500).json({ error: "Internal Server Error"})
     }
 })
+
+//Logout
+router.post('/api/v1/logout', async(req, res) => {
+    // Extract the access token from the authorization header
+  const authorizationHeader = req.headers['authorization'];
+  const accessToken = authorizationHeader.split(' ')[1];
+
+  // Verify the access token
+  const secretKey = 'my-secret-key';
+  const decodedAccessToken = jwt.verify(accessToken, secretKey);
+
+  if (!decodedAccessToken) {
+    return res.status(401).json({ message: 'Invalid access token' });
+  }
+
+  const userId = decodedAccessToken.userId;
+
+  // Invalidate the access token by blacklisting it
+  blacklistedTokens.push(accessToken);
+
+  // Send a logout response
+  res.json({ message: 'Logged out successfully' });
+})
+
+// // Google Authentication
+// // Login Failed
+// router.get('/api/v1/login/failed', (req, res) => {
+//     res.status(401).json({
+//         error: true, 
+//         message: "Login Failed"
+//     })
+// })
+// // Handle authentication flow
+// router.get('/api/v1/login', passport.authenticate('google', {
+//     successRedirect: '/profile',
+//     failureRedirect: '/login',
+//   })
+// )
+// // Protect routes
+// router.get('/profile', isLoggedIn, (req, res) => {
+//     // Display user profile information
+//     const user = req.user;
+//     res.render('profile', { user });
+// });
+  
+// // Check if user is logged in
+// function isLoggedIn(req, res, next) {
+//     if (!req.isAuthenticated()) {
+//         return res.redirect('/login');
+//     }
+//     next();
+// }
+  
+//   // Handle login failure
+// router.get('/login', (req, res) => {
+//     const error_message = req.flash('error_message');
+//     res.render('login', { error_message });
+// });
